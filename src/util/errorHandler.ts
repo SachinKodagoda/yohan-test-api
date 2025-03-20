b44f93736b25a0b6f26d4af68b69a78fb0e93530
Duminda Kodagoda <duminda.g.k@gmail.com>
2025-03-21 01:55:49 +0530

    this is a test

--- src/util/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Response, Request } from "express";
-import HttpStatusCodes from "./statusCodes.ts";
-
-export const handleError = (res: Response, error: any) => {
-  res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: "Internal Server Error...",
-  });
-};
